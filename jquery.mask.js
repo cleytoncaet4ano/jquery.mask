50a16f3a4fd104720735273f75a3e2d60aee89b0
cleytoncaet4ano <129920867+cleytoncaet4ano@users.noreply.github.com>
2023-04-05 01:50:19 -0300

    Update jquery.mask.js
    Atualização para receber funções como parâmetro, afim de determinar diferentes tipos de formatos de mascara.

--- jquery.mask.js
+++ jquery.mask.js
@@ -1,22 +1,23 @@
-	$.fn.mask = function (format) {
-		return this.on('input', function () {
-			let inputValue = $(this).val().replace(/\D/g, '');
-			let formattedValue = '';
-			let j = 0;
-			
-			for (let i = 0; i < format.length; i++) {
-				if (format[i] === '0' && inputValue[j]) {
-					formattedValue += inputValue[j];
-					j++;
-				} else if (format[i] !== '0') {
-					formattedValue += format[i];
-				}
-				
-				if (j >= inputValue.length) {
-					break;
-				}
-			}
-			
-			$(this).val(formattedValue);
-		});
-	};
+$.fn.mask = function (formatOrFunction) {
+    return this.on('input', function () {
+        let inputValue = $(this).val().replace(/\D/g, '');
+        let format = typeof formatOrFunction === 'function' ? formatOrFunction(inputValue) : formatOrFunction;
+        let formattedValue = '';
+        let j = 0;
+
+        for (let i = 0; i < format.length; i++) {
+            if (format[i] === '0' && inputValue[j]) {
+                formattedValue += inputValue[j];
+                j++;
+            } else if (format[i] !== '0') {
+                formattedValue += format[i];
+            }
+
+            if (j >= inputValue.length) {
+                break;
+            }
+        }
+
+        $(this).val(formattedValue);
+    });
+};
